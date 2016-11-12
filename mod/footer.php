6e7a0fa0541d5c097292092a27da946a36670f66
alexandrpunk <alexandrpunk@gmail.com>
2016-11-11 18:50:50 -0700

    sincroniza los cambios para le cambio de equipo

--- mod/footer.php
+++ mod/footer.php
@@ -1,20 +1,20 @@
 <div class="footer">
     <div class="col-md-4 col-sm-4">
-        <img src="img/japi.svg" class=" center-block logo img-responsive">
+        <img src="img/japi.svg" class="center-block logo img-responsive">
     </div>
-    <div class="col-md-4 col-sm-4">
+    <div class="col-md-4 col-sm-4 ">
+        <address class="hidden-sm hidden-xs">
+            Frida Kahlo 2411 Nte., Fracc. Rincón Alameda,<br>
+            Culiacán, Sinaloa, México.
+        </address>
+        <strong class="copyright">© 2016 Manitas Unidas I.A.P.</strong>
+    </div>
+    <div class="col-md-4 col-sm-4 hidden-sm hidden-xs">
         <address>
-                Frida Kahlo 2411 Nte., Fracc. Rincón Alameda,<br>
-                Culiacán, Sinaloa, México.<br>
-                <abbr title="Telefonos"><strong>Tels:</strong></abbr> (667) 715 2166 | 715-1714<br>
-                <strong>Lada sin costo:</strong> 01 800 509 1985
-            </address>
+        <abbr title="Telefonos"><strong>Tels:</strong></abbr> (667) 715 2166 | 715-1714<br>
+        <strong>Lada sin costo:</strong> 01 800 509 1985<br>
+        <a href="mailto:info@manitasunidas.org" target="_blank">info@manitasunidas.org</a>
+        </address>
     </div>
-    <div class="col-md-4 col-sm-4 socials">
-        <p><i class="fa fa-envelope-square fa-lg"></i> info@manitasunidas.org.mx</p>
-        <p><i class="fa fa-facebook-official fa-lg"></i> Manitas Unidas IAP</p>
-        <p><i class="fa fa-twitter-square fa-lg"></i> @manitasunidasIAP</p>
-    </div>
-    <p class="col-md-12 text-center copyright"><strong>© 2016 Manitas Unidas I.A.P.</strong></p>
 </div>
 <?php require_once 'scripts.php';?>
